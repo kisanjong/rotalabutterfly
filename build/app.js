28fd3935ed2b9f745ec1fc752813b686453208d5
Jennifer Scroggins <jennifer@ravenwilde.com>
2015-06-15 12:19:03 -0400

    jsx watch via node no in place

---
+++ build/app.js
@@ -0,0 +1,13 @@
+var NutrientCalculator = React.createClass({displayName: "NutrientCalculator",
+	render: function() {
+		return (
+			React.createElement("h1", null, "HELP")
+		);
+	},
+});
+
+React.render(
+	React.createElement(NutrientCalculator, null),
+  	document.getElementById('example')
+);
+
---
+++ js/app.js
@@ -0,0 +1,13 @@
+var NutrientCalculator = React.createClass({
+	render: function() {
+		return (
+			<h1>HELP</h1>
+		);
+	},
+});
+
+React.render(
+	<NutrientCalculator />,
+  	document.getElementById('example')
+);
+
